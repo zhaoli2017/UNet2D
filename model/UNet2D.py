849d3d86597262d78864b7313055cfee316e7fbc
Zhao Li <Zhao.Li@uth.tmc.edu>
2020-07-04 15:00:25 -0500

    new implementation for  UNet2D.py

--- model/UNet2D.py
+++ model/UNet2D.py
@@ -1,129 +1,117 @@
 import torch
 import torch.nn as nn
 
-def conv_block_2d(in_dim, out_dim, activation):
-    return nn.Sequential(
-        nn.Conv2d(in_dim, out_dim, kernel_size=3, stride=1, padding=1),
-        nn.BatchNorm2d(out_dim),
-        activation, )
-
-
-def conv_trans_block_2d(in_dim, out_dim, activation):
-    return nn.Sequential(
-        nn.ConvTranspose2d(in_dim, out_dim, kernel_size=3, stride=2, padding=1, output_padding=1),
-        nn.BatchNorm2d(out_dim),
-        activation, )
-
-
-def max_pooling_2d():
-    return nn.MaxPool2d(kernel_size=2, stride=2, padding=0)
-
-
-def conv_block_2_2d(in_dim, out_dim, activation):
-    return nn.Sequential(
-        conv_block_2d(in_dim, out_dim, activation),
-        nn.Conv2d(out_dim, out_dim, kernel_size=3, stride=1, padding=1),
-        nn.BatchNorm2d(out_dim), )
-
-
 class UNet2D(nn.Module):
-    def __init__(self, in_dim=1, out_dim=3, num_filters=4):
+    def __init__(self, in_channel=1, n_classes=3):
+        self.in_channel = in_channel
+        self.n_classes = n_classes
         super(UNet2D, self).__init__()
+        bias_flag = False
+        bn_flag = True
+        self.ec0 = self.encoder(self.in_channel, 32, bias=bias_flag, batchnorm=bn_flag)
+        self.ec1 = self.encoder(32, 64, bias=bias_flag, batchnorm=bn_flag)
+        self.ec2 = self.encoder(64, 64, bias=bias_flag, batchnorm=bn_flag)
+        self.ec3 = self.encoder(64, 128, bias=bias_flag, batchnorm=bn_flag)
+        self.ec4 = self.encoder(128, 128, bias=bias_flag, batchnorm=bn_flag)
+        self.ec5 = self.encoder(128, 256, bias=bias_flag, batchnorm=bn_flag)
+        self.ec6 = self.encoder(256, 256, bias=bias_flag, batchnorm=bn_flag)
+        self.ec7 = self.encoder(256, 512, bias=bias_flag, batchnorm=bn_flag)
 
-        self.in_dim = in_dim
-        self.out_dim = out_dim
-        self.num_filters = num_filters
-        activation = nn.LeakyReLU(0.2, inplace=True)
+        self.pool0 = nn.MaxPool2d(2)
+        self.pool1 = nn.MaxPool2d(2)
+        self.pool2 = nn.MaxPool2d(2)
 
-        # Down sampling
-        self.down_1 = conv_block_2_2d(self.in_dim, self.num_filters, activation)
-        self.pool_1 = max_pooling_2d()
-        self.down_2 = conv_block_2_2d(self.num_filters, self.num_filters * 2, activation)
-        self.pool_2 = max_pooling_2d()
-        self.down_3 = conv_block_2_2d(self.num_filters * 2, self.num_filters * 4, activation)
-        self.pool_3 = max_pooling_2d()
-        self.down_4 = conv_block_2_2d(self.num_filters * 4, self.num_filters * 8, activation)
-        self.pool_4 = max_pooling_2d()
-        self.down_5 = conv_block_2_2d(self.num_filters * 8, self.num_filters * 16, activation)
-        self.pool_5 = max_pooling_2d()
+        self.dc9 = self.decoder(512, 512, kernel_size=2, stride=2, bias=bias_flag)
+        self.dc8 = self.decoder(256 + 512, 256, kernel_size=3, stride=1, padding=1, bias=bias_flag)
+        self.dc7 = self.decoder(256, 256, kernel_size=3, stride=1, padding=1, bias=bias_flag)
+        self.dc6 = self.decoder(256, 256, kernel_size=2, stride=2, bias=bias_flag)
+        self.dc5 = self.decoder(128 + 256, 128, kernel_size=3, stride=1, padding=1, bias=bias_flag)
+        self.dc4 = self.decoder(128, 128, kernel_size=3, stride=1, padding=1, bias=bias_flag)
+        self.dc3 = self.decoder(128, 128, kernel_size=2, stride=2, bias=bias_flag)
+        self.dc2 = self.decoder(64 + 128, 64, kernel_size=3, stride=1, padding=1, bias=bias_flag)
+        self.dc1 = self.decoder(64, 64, kernel_size=3, stride=1, padding=1, bias=bias_flag)
+        self.dc0 = self.decoder(64, n_classes, kernel_size=1, stride=1, bias=bias_flag)
 
-        # Bridge
-        self.bridge = conv_block_2_2d(self.num_filters * 16, self.num_filters * 32, activation)
+        self.softmax = nn.Softmax(dim=1)
 
-        # Up sampling
-        self.trans_1 = conv_trans_block_2d(self.num_filters * 32, self.num_filters * 32, activation)
-        self.up_1 = conv_block_2_2d(self.num_filters * 48, self.num_filters * 16, activation)
-        self.trans_2 = conv_trans_block_2d(self.num_filters * 16, self.num_filters * 16, activation)
-        self.up_2 = conv_block_2_2d(self.num_filters * 24, self.num_filters * 8, activation)
-        self.trans_3 = conv_trans_block_2d(self.num_filters * 8, self.num_filters * 8, activation)
-        self.up_3 = conv_block_2_2d(self.num_filters * 12, self.num_filters * 4, activation)
-        self.trans_4 = conv_trans_block_2d(self.num_filters * 4, self.num_filters * 4, activation)
-        self.up_4 = conv_block_2_2d(self.num_filters * 6, self.num_filters * 2, activation)
-        self.trans_5 = conv_trans_block_2d(self.num_filters * 2, self.num_filters * 2, activation)
-        self.up_5 = conv_block_2_2d(self.num_filters * 3, self.num_filters * 1, activation)
 
-        # Output
-        self.out = conv_block_2d(self.num_filters, out_dim, nn.Softmax(dim=1))
+    def encoder(self, in_channels, out_channels, kernel_size=3, stride=1, padding=1,
+                bias=True, batchnorm=True):
+        if batchnorm:
+            layer = nn.Sequential(
+                nn.Conv2d(in_channels, out_channels, kernel_size, stride=stride, padding=padding, bias=bias),
+                nn.BatchNorm2d(out_channels),
+                nn.ReLU())
+        else:
+            layer = nn.Sequential(
+                nn.Conv2d(in_channels, out_channels, kernel_size, stride=stride, padding=padding, bias=bias),
+                nn.ReLU())
+        return layer
+
+
+    def decoder(self, in_channels, out_channels, kernel_size, stride=1, padding=0,
+                output_padding=0, bias=True):
+        layer = nn.Sequential(
+            nn.ConvTranspose2d(in_channels, out_channels, kernel_size, stride=stride,
+                               padding=padding, output_padding=output_padding, bias=bias),
+            nn.ReLU())
+        return layer
 
     def forward(self, x):
-        # Down sampling
-        down_1 = self.down_1(x)  # -> [1, 4, 128, 128, 128]
-        pool_1 = self.pool_1(down_1)  # -> [1, 4, 64, 64, 64]
+        e0 = self.ec0(x)
+        syn0 = self.ec1(e0)
+        e1 = self.pool0(syn0)
+        e2 = self.ec2(e1)
+        syn1 = self.ec3(e2)
+        del e0, e1, e2
 
-        down_2 = self.down_2(pool_1)  # -> [1, 8, 64, 64, 64]
-        pool_2 = self.pool_2(down_2)  # -> [1, 8, 32, 32, 32]
+        e3 = self.pool1(syn1)
+        e4 = self.ec4(e3)
+        syn2 = self.ec5(e4)
+        del e3, e4
 
-        down_3 = self.down_3(pool_2)  # -> [1, 16, 32, 32, 32]
-        pool_3 = self.pool_3(down_3)  # -> [1, 16, 16, 16, 16]
+        e5 = self.pool2(syn2)
+        e6 = self.ec6(e5)
+        e7 = self.ec7(e6)
+        del e5, e6
 
-        down_4 = self.down_4(pool_3)  # -> [1, 32, 16, 16, 16]
-        pool_4 = self.pool_4(down_4)  # -> [1, 32, 8, 8, 8]
+        d9 = torch.cat((self.dc9(e7), syn2), dim=1)
+        del e7, syn2
 
-        down_5 = self.down_5(pool_4)  # -> [1, 64, 8, 8, 8]
-        pool_5 = self.pool_5(down_5)  # -> [1, 64, 4, 4, 4]
+        d8 = self.dc8(d9)
+        d7 = self.dc7(d8)
+        del d9, d8
 
-        # Bridge
-        bridge = self.bridge(pool_5)  # -> [1, 128, 4, 4, 4]
+        d6 = torch.cat((self.dc6(d7), syn1), dim=1)
+        del d7, syn1
 
-        # Up sampling
-        trans_1 = self.trans_1(bridge)  # -> [1, 128, 8, 8, 8]
-        concat_1 = torch.cat([trans_1, down_5], dim=1)  # -> [1, 192, 8, 8, 8]
-        up_1 = self.up_1(concat_1)  # -> [1, 64, 8, 8, 8]
+        d5 = self.dc5(d6)
+        d4 = self.dc4(d5)
+        del d6, d5
 
-        trans_2 = self.trans_2(up_1)  # -> [1, 64, 16, 16, 16]
-        concat_2 = torch.cat([trans_2, down_4], dim=1)  # -> [1, 96, 16, 16, 16]
-        up_2 = self.up_2(concat_2)  # -> [1, 32, 16, 16, 16]
+        d3 = torch.cat((self.dc3(d4), syn0), dim=1)
+        del d4, syn0
 
-        trans_3 = self.trans_3(up_2)  # -> [1, 32, 32, 32, 32]
-        concat_3 = torch.cat([trans_3, down_3], dim=1)  # -> [1, 48, 32, 32, 32]
-        up_3 = self.up_3(concat_3)  # -> [1, 16, 32, 32, 32]
+        d2 = self.dc2(d3)
+        d1 = self.dc1(d2)
+        del d3, d2
 
-        trans_4 = self.trans_4(up_3)  # -> [1, 16, 64, 64, 64]
-        concat_4 = torch.cat([trans_4, down_2], dim=1)  # -> [1, 24, 64, 64, 64]
-        up_4 = self.up_4(concat_4)  # -> [1, 8, 64, 64, 64]
+        d0 = self.dc0(d1)
+        return self.softmax(d0)
 
-        trans_5 = self.trans_5(up_4)  # -> [1, 8, 128, 128, 128]
-        concat_5 = torch.cat([trans_5, down_1], dim=1)  # -> [1, 12, 128, 128, 128]
-        up_5 = self.up_5(concat_5)  # -> [1, 4, 128, 128, 128]
-
-        # Output
-        out = self.out(up_5)  # -> [1, 3, 128, 128, 128]
-        return out
-
-
-if __name__ == "__main__":
+if __name__ == '__main__':
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+    device = 'cpu'
     image_size = 128
-    #x = torch.Tensor(1, 1, image_size, image_size, image_size)
-    x = torch.Tensor(1, 1, 128, 128)
+    x = torch.Tensor(1, 1, image_size, image_size)
     x.to(device)
     print("x size: {}".format(x.size()))
 
-    model = UNet2D(in_dim=1, out_dim=3, num_filters=4)
+    model = UNet2D(1, 3)
 
     out = model(x)
     print("out size: {}".format(out.size()))
 
     from torchsummary import summary
     print(model)
-    summary(model.cuda(), (1, 256, 256))
+    summary(model.to(device), (1, 256, 256), device=device)
